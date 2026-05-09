3ade0acb7d6ef6fa804c1219f4e99c28d787e6de
wassimloussif <Wassim.LOUSSIF@esprit.tn>
2026-05-09 15:07:56 +0100

    Correction du login et register

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -11,7 +11,7 @@ use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
-    public function showLogin()
+    public function showLoginnn()
     {
         return view('auth.login');
     }
